85920a34b437bd08d18db713c20ab1135c173824
j9han1017 <johandiaz101706@gmail.com>
2024-03-29 23:05:50 -0500

    promise actualizado

--- src/callback/promise.js
+++ src/callback/promise.js
@@ -14,3 +14,9 @@ const countCows = new Promise(function (resolve, reject) {
     }
 })
 
+
+countCows.then((result) => {
+    console.log(result)
+}).catch((error) => {
+    console.log(error);
+}).finally(() => console.log("Finally"))
