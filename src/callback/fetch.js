3ec81c8de8d5bc67464696a3b80128286c265d55
j9han1017 <johandiaz101706@gmail.com>
2024-03-31 13:06:55 -0500

    fetch actualizado

--- src/callback/fetch.js
+++ src/callback/fetch.js
@@ -5,12 +5,28 @@ function fetchData(urlApi) {
     return fetch(urlApi)
 };
 
+// fetchData(`${API}/products`)
+// .then(response => response.json())
+// .then(products => {
+//     console.log(products);
+// })
+// .then(() => {
+//     console.log('hola')
+// })
+// .catch(error => console.log(error))
+
 fetchData(`${API}/products`)
 .then(response => response.json())
 .then(products => {
-    console.log(products);
+    return fetchData(`${API}/products/${products[0].id}`)
 })
-.then(() => {
-    console.log('hola')
+.then(response => response.json())
+.then(product => {
+    return fetchData(`${API}/categories/${product.category.id}`)
 })
-.catch(error => console.log(error))
+.then(response => response.json())
+.then(category => {
+    console.log(category.name)
+})
+.catch(err => console.log(err))
+.finally(() => console.log('Finally'))
