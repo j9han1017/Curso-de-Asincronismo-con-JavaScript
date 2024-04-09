2846a74d5b64d30d2b10e34cc1ea03bd216dc0e6
j9han1017 <johandiaz101706@gmail.com>
2024-04-08 20:17:42 -0500

    Actualizacion main

--- src/proyecto/assets/main.js
+++ src/proyecto/assets/main.js
@@ -1,26 +1,6 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
-
-const content = null || document.getElementById('content');
-
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-    'X-RapidAPI-Key': 'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'
-  }
-};
-
-async function fetchData(urlApi) {
-  const response = await fetch(urlApi, options);
-  const data = await response.json();
-  return data;
-}
-
-(async () => {
-  try {
-    const videos = await fetchData(API);
-    let view = `
-    ${videos.items.map(video => `
+const API='https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';const content=null||document.getElementById('content');const options={method:'GET',headers:{'X-RapidAPI-Host':'youtube-v31.p.rapidapi.com','X-RapidAPI-Key':'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'}};async function fetchData(urlApi){const response=await fetch(urlApi,options);const data=await response.json();return data;}
+(async()=>{try{const videos=await fetchData(API);let view=`
+    ${videos.items.map(video=>`
       <div class="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
@@ -35,8 +15,4 @@ async function fetchData(urlApi) {
       </div>
     `).slice(0,4).join('')}
 
-    `;
-  } catch {
-
-  }
-})();
+    `;}catch{}})();
